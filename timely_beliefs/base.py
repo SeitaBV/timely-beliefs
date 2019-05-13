1d5cc11afd67d7e9a8f53335c36787b57c599760
Nicolas Höning <nicolas@seita.nl>
2019-05-13 17:49:54 +0200

    Add improved to_pypi script which handles dev versioning

--- timely_beliefs/base.py
+++ timely_beliefs/base.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# This engine and session should move to tests, only useful there. Base is needed by packages using tb,
+# but we might put it in the index.
 engine = create_engine("postgresql://tbtest:tbtest@127.0.0.1/tbtest")
 Session = sessionmaker(bind=engine)
 session = Session()
---
+++ to_pypi.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf dist/*
+
+pip -q install twine
+
+python setup.py -q alias -u release egg_info -Db ""
+
+if [ "$1" = "dev" ]; then
+    if [ "$2" != "" ]; then
+        dev_number=$2
+        cleandevtag=${dev_number//[0-9]/}  # only numbers
+        if [ "$cleandevtag" != "$dev_number" ]; then
+            echo "Only numbers in the dev tag please!"
+            exit 2
+        fi
+        echo "Using $dev_number as dev build number ..."
+    else
+        revisioncount=`git log --oneline | wc -l`
+        projectversion=`git describe --tags --always --long`
+        cleanversion=${projectversion//[a-z]/}  # only numbers
+        dev_number=$cleanversion$revisioncount
+        echo "Using $dev_number as dev build number ..."
+    fi
+
+    python setup.py -q alias -u dev egg_info --tag-build=-dev$dev_number
+    python setup.py dev sdist
+    python setup.py dev bdist_wheel
+elif [ "$1" = "release" ]; then
+    python setup.py release sdist
+    python setup.py release bdist_wheel
+else
+    echo "Argument needs to be release or dev"
+    exit 2
+fi
+
+twine upload dist/*
