f3ae9f7cadf0f1988a20d7dd6d60c7a34008f425
Felix Claessen <felix@seita.nl>
2020-05-09 11:48:55 +0200

    Subclass DBBeliefSource.

--- timely_beliefs/sources/classes.py
+++ timely_beliefs/sources/classes.py
@@ -24,7 +24,7 @@ class BeliefSource(object):
         return self.name < other.name
 
 
-class DBBeliefSource(Base):
+class DBBeliefSource(Base, BeliefSource):
     """Mixin class for a table with belief sources, i.e. data-creating entities such as users or scripts."""
 
     __tablename__ = "belief_source"
@@ -38,7 +38,8 @@ class DBBeliefSource(Base):
     name = Column(String(120), nullable=False, default="")
 
     def __init__(self, name: str):
-        self.name = name
+        BeliefSource.__init__(self, name)
+        Base.__init__(self)
 
     def __repr__(self):
         return "<DBBeliefSource %s (%s)>" % (self.id, self.name)
