650851fb28f777aa5d544064f8a060958a9f02cd
Felix Claessen <felix@seita.nl>
2020-07-09 16:47:41 +0200

    Add BeliefsSeries as a main import class.

--- timely_beliefs/__init__.py
+++ timely_beliefs/__init__.py
@@ -2,6 +2,11 @@
 
 from timely_beliefs.sensors.classes import Sensor, DBSensor
 from timely_beliefs.sources.classes import BeliefSource, DBBeliefSource
-from timely_beliefs.beliefs.classes import TimedBelief, DBTimedBelief, BeliefsDataFrame
+from timely_beliefs.beliefs.classes import (
+    BeliefsDataFrame,
+    BeliefsSeries,
+    DBTimedBelief,
+    TimedBelief,
+)
 from timely_beliefs.beliefs.utils import read_csv, load_time_series
 from timely_beliefs.examples import beliefs_data_frames
