b9452ccf08b7fe7fa0a687dd03cd45b5cfa043d1
Nicolas Höning <nicolas@seita.nl>
2020-08-06 21:36:19 +0200

    isort:ignore in bvp/__init__.py for correct import order

--- timely_beliefs/__init__.py
+++ timely_beliefs/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa
 
+from timely_beliefs.sensors.classes import DBSensor, Sensor  # isort:skip
+from timely_beliefs.sources.classes import BeliefSource, DBBeliefSource  # isort:skip
 from timely_beliefs.beliefs.classes import (
     BeliefsDataFrame,
     BeliefsSeries,
@@ -8,5 +10,3 @@ from timely_beliefs.beliefs.classes import (
 )
 from timely_beliefs.beliefs.utils import load_time_series, read_csv
 from timely_beliefs.examples import beliefs_data_frames
-from timely_beliefs.sensors.classes import DBSensor, Sensor
-from timely_beliefs.sources.classes import BeliefSource, DBBeliefSource
