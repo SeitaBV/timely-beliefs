a56396a3544a75a400778e641b4829cab08612c0
F.N. Claessen <felix@seita.nl>
2020-11-27 11:50:35 +0100

    Release version 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "uncertainty",
         "lineage",
     ],
-    version="0.1.2",
+    version="0.1.3",
     install_requires=[
         "pytz",
         "pandas>=0.24,<1.1",  # test_groupby_preserves_metadata fails on 1.1
