a1667bf7113af26481ae1b36cef40297cf832531
Nicolas Höning <nicolas@seita.nl>
2021-12-01 20:56:37 +0100

    we're not certain enough to offcially support Python 3.6

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Development Status :: 3 - Alpha",
