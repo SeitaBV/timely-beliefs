e5ae6c5516ea4ac55594992ff2c865baa7146d78
F.N. Claessen <felix@seita.nl>
2022-01-07 13:38:47 +0100

    Allow passing existing query

--- timely_beliefs/beliefs/queries.py
+++ timely_beliefs/beliefs/queries.py
@@ -1,13 +1,19 @@
+from typing import Optional
+
 from sqlalchemy.orm import Query, Session
 from sqlalchemy import and_, func
 
 
-def query_unchanged_beliefs(session: Session, cls: "TimedBeliefDBMixin") -> Query:
+def query_unchanged_beliefs(
+    session: Session, cls: "TimedBeliefDBMixin", query: Optional[Query] = None
+) -> Query:
     """Match unchanged beliefs.
 
     Unchanged beliefs are beliefs that have already been recorded with an earlier belief time,
     and are otherwise the same.
     """
+    if query is None:
+        query = cls.query
     subq = (
         session.query(
             cls.event_start,
@@ -19,7 +25,7 @@ def query_unchanged_beliefs(session: Session, cls: "TimedBeliefDBMixin") -> Quer
         .group_by(cls.event_start, cls.sensor_id, cls.source_id, cls.event_value)
         .subquery()
     )
-    q = cls.query.join(
+    q = query.join(
         subq,
         and_(
             cls.event_start == subq.c.event_start,
