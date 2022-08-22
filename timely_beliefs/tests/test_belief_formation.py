302850c77fd35f628888f2be613321f3b77ee59f
F.N. Claessen <felix@seita.nl>
2022-08-22 16:56:29 +0200

    Add test for forming a single new belief

--- timely_beliefs/tests/test_belief_formation.py
+++ timely_beliefs/tests/test_belief_formation.py
@@ -1,3 +1,5 @@
+import pytest
+
 from datetime import datetime
 import pytz
 
@@ -8,7 +10,16 @@ from timely_beliefs import BeliefSource
 from timely_beliefs.examples import get_example_df
 
 
-def test_form_single_belief():
+@pytest.mark.parametrize(
+    "forecaster, forecast",
+    [
+        # by default, most recent belief by Source A about most recent event
+        (None, 400),
+        # mean value of most recent beliefs by Source A about previous events
+        (NaiveForecaster(strategy="mean"), 250),
+    ],
+)
+def test_form_single_belief(forecaster, forecast):
     df = get_example_df()
     print(df)
     df_in = df[df.index.get_level_values("source") == df.lineage.sources[0]]
@@ -19,18 +30,8 @@ def test_form_single_belief():
         belief_time=datetime(2000, 1, 1, 2, tzinfo=pytz.utc),
         source=BeliefSource("Source C"),
         event_start=datetime(2000, 1, 3, 13, tzinfo=pytz.utc),
+        forecaster=forecaster,
     )
-    assert (
-        df_out.values[0] == 400
-    )  # most recent belief by Source A about most recent event
-    df_out = df_in.form_beliefs(
-        belief_time=datetime(2000, 1, 1, 2, tzinfo=pytz.utc),
-        source=BeliefSource("Source C"),
-        event_start=datetime(2000, 1, 3, 13, tzinfo=pytz.utc),
-        forecaster=NaiveForecaster(strategy="mean"),
-    )
-    assert (
-        df_out.values[0] == 250
-    )  # mean value of most recent beliefs by Source A about previous events
+    assert df_out.values[0] == forecast
     df = pd.concat([df, df_out])
     print(df)
