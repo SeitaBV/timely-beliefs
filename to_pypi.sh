aea725991a879f41dfa8226501af29f7087db4a1
F.N. Claessen <felix@seita.nl>
2022-09-05 16:18:53 +0200

    Stop --dry-run flag from exiting the terminal and fix the flag's docs

--- to_pypi.sh
+++ to_pypi.sh
@@ -10,7 +10,7 @@
 # Usage
 # ------------
 #
-# ./to_pypi [--dry-run]
+# ./to_pypi --dry-run
 #
 # If the --dry-run flag is present, this script will do all steps, but skip the upload to Pypi.
 # 
@@ -38,7 +38,7 @@ python setup.py egg_info bdist_wheel
 
 if [ "$1" == "--dry-run" ]; then
     echo "[TO_PYPI] Not uploading to Pypi (--dry-run active) ..."
-    exit
+else
+    echo "[TO_PYPI] Uploading to Pypi ..."
+    twine upload dist/*
 fi
-echo "[TO_PYPI] Uploading to Pypi ..."
-twine upload dist/*
