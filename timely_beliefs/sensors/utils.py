6260308975b8304bbb298dec5caea8e02da52e19
F.N. Claessen <felix@seita.nl>
2022-10-10 11:03:21 +0200

    Fix iso formatting of datetime value

--- timely_beliefs/sensors/utils.py
+++ timely_beliefs/sensors/utils.py
@@ -7,7 +7,7 @@ from isodate import (
     duration_isoformat,
     parse_datetime,
     parse_duration,
-    time_isoformat,
+    datetime_isoformat,
 )
 from pytz import timezone
 
@@ -43,7 +43,7 @@ def jsonify_time_dict(d: dict) -> dict:
     d2 = {}
     for k, v in d.items():
         if isinstance(v, datetime):
-            d2[k] = time_isoformat(v)
+            d2[k] = datetime_isoformat(v)
         elif isinstance(v, timedelta):
             d2[k] = duration_isoformat(v)
         else:
