fac47b4447fa0b198e36abad63966d15a84b5311
F.N. Claessen <felix@seita.nl>
2023-06-15 15:02:52 +0200

    fix: load function store upon import, with potentially a big speed-up as a result
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- timely_beliefs/sensors/utils.py
+++ timely_beliefs/sensors/utils.py
@@ -46,10 +46,14 @@ def unjsonify_time_dict(d: dict) -> dict:
     return d2
 
 
-def get_func_store() -> dict:
-    """Returns a dictionary with Callable objects supported in our function store,
-    indexed by their function names."""
-    return {o[0]: o[1] for o in getmembers(knowledge_horizons) if isfunction(o[1])}
+# A dictionary with function specification (incl. Callable objects) supported in our function store, indexed by their function names.
+FUNC_STORE: dict = {
+    o[0]: {
+        "fnc": o[1],
+        "args": getfullargspec(o[1]).args,
+    }
+    for o in getmembers(knowledge_horizons, isfunction)
+}
 
 
 def eval_verified_knowledge_horizon_fnc(
@@ -63,11 +67,10 @@ def eval_verified_knowledge_horizon_fnc(
     Only function names that represent Callable objects in our function store can be evaluated.
     If get_bounds is True, a tuple is returned with bounds on the possible return value.
     """
-    for verified_fnc_name, verified_fnc in get_func_store().items():
+    for verified_fnc_name, verified_fnc_specs in FUNC_STORE.items():
+        verified_fnc = verified_fnc_specs["fnc"]
         if verified_fnc_name == requested_fnc_name:
-            if {"event_start", "event_resolution"} < set(
-                getfullargspec(verified_fnc).args
-            ):
+            if {"event_start", "event_resolution"} < set(verified_fnc_specs["args"]):
                 # Knowledge horizons are anchored to event_end = event_start + event_resolution
                 return verified_fnc(
                     event_start,
@@ -75,12 +78,12 @@ def eval_verified_knowledge_horizon_fnc(
                     **(unjsonify_time_dict(par)),
                     get_bounds=get_bounds
                 )
-            elif "event_start" in getfullargspec(verified_fnc).args:
+            elif "event_start" in verified_fnc_specs["args"]:
                 # Knowledge horizons are anchored to event_start
                 return verified_fnc(
                     event_start, **(unjsonify_time_dict(par)), get_bounds=get_bounds
                 )
-            elif "event_resolution" in getfullargspec(verified_fnc).args:
+            elif "event_resolution" in verified_fnc_specs["args"]:
                 # Knowledge horizons are anchored to event_start
                 return verified_fnc(
                     event_resolution,
