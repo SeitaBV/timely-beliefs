af3ee7cf1fe4ffbb2f2e64fa4cb46d68ed26e099
F.N. Claessen <felix@seita.nl>
2025-12-12 14:27:37 +0100

    fix: pass on session in recursive call
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- timely_beliefs/beliefs/utils.py
+++ timely_beliefs/beliefs/utils.py
@@ -1255,8 +1255,8 @@ def drop_unchanged_beliefs(
     ex_post_bdf = bdf[bdf.belief_horizons <= timedelta(0)]
     if not ex_ante_bdf.empty and not ex_post_bdf.empty:
         # We treat each part separately to avoid that ex-post knowledge would be lost
-        ex_ante_bdf = drop_unchanged_beliefs(ex_ante_bdf)
-        ex_post_bdf = drop_unchanged_beliefs(ex_post_bdf)
+        ex_ante_bdf = drop_unchanged_beliefs(ex_ante_bdf, session=session)
+        ex_post_bdf = drop_unchanged_beliefs(ex_post_bdf, session=session)
         bdf = pd.concat([ex_ante_bdf, ex_post_bdf])
         return bdf
 
